f3f45a3efff67aa789d8e2f80e85cf3145456674
Rich <rtran3102@gmail.com>
2023-08-01 15:56:03 -0400

    add phrases

--- wordGen.js
+++ wordGen.js
@@ -3,7 +3,21 @@ const randomNum = () =>{
     num = (Math.floor(Math.random() * 3));
     return num;
 }
-
+console.log(randomNum());
 const phrase = (funct) =>{
-    
+    let number = randomNum();
+    switch (number){
+        case 0:
+            return "Hello World";
+            break;
+        case 1:
+            return "Good World";
+            break;
+        case 2:
+            return "Bye World";
+            break;
+        default:
+            return "bruh";
+            break;
+    }
 } 
